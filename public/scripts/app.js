e7ecd1312748ce1b81ca916b721495895235cc55
steveabouem <steveabouem@gmail.com>
2018-05-03 14:54:55 +0000

    additional css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,44 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(users => {
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
     }
-  });;
+  });
 });
+
+$(document).ready(function() {
+  //we will use variables in this  event listener to fill in every paragraph(which will contain
+  //the new entries)
+  $(".categories").on("click", ".movies", function() {
+    let displayTodos = `<div class="movieList">
+  <p>latest entry to this category<p>
+  </div>`;
+    // $(".movies").toggle(displayTodos); trying ot make the list slide down, doesnt work.
+    $(".movies").append(displayTodos);
+  });
+
+  $(".categories").on("click", ".food", function() {
+    let displayTodos = `<div class="foodList">
+<p>latest entry to this category<p>
+</div>`;
+    $(".food").append(displayTodos);
+  });
+
+  $(".categories").on("click", ".merch", function() {
+    let displayTodos = `<div class="merchList">
+<p>latest entry to this category<p>
+</div>`;
+    $(".merch").append(displayTodos);
+  });
+
+  $(".categories").on("click", ".books", function() {
+    let displayTodos = `<div class="booksList">
+<p>latest entry to this category<p>
+</div>`;
+    $(".books").append(displayTodos);
+  });
+}); //document ready function
