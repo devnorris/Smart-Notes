85f580533f0f083e5ffb6738795d7d1ef8e24262
steveabouem <steveabouem@gmail.com>
2018-05-05 02:02:21 +0000

    scope issue when running all 4 verif. They work separately but not together

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,33 @@
-$(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done(users => {
-  //   for (user of users) {
-  //     $("<div>")
-  //       .text(user.name)
-  //       .appendTo($("body"));
-  //   }
+// $(() => {
+//   // $.ajax({
+//   //   method: "GET",
+//   //   url: "/api/users"
+//   // }).done(users => {
+//   //   for (user of users) {
+//   //     $("<div>")
+//   //       .text(user.name)
+//   //       .appendTo($("body"));
+//   //   }
 
-  $("form").on("submit", function(event) {
-    const formData = $("form").serialize();
-    event.preventDefault();
-    $.get("/smart", function(response) {
-      $.ajax({
-        url: "/smart",
-        method: "POST",
-        data: formData,
-        success: function(result) {
-          console.log("result: ", result);
-          $(".bookDisplay").append(result);
-        },
-        error: function(error) {
-          console.log("ajax call fail");
-        }
-      });
-    });
-  });
-});
+//   $("form").on("submit", function(event) {
+//     const formData = $("form").serialize();
+//     event.preventDefault();
+//     $.get("/smart", function(response) {
+//       $.ajax({
+//         url: "/smart",
+//         method: "POST",
+//         data: formData,
+//         success: function(result) {
+//           console.log("result: ", result);
+//           if (result.class === "foodDisplay") {
+//             $(".foodDisplay").append(result.value);
+//           }
+//         },
+//         error: function(error) {
+//           console.log("ajax call fail");
+//         }
+//       });
+//     });
+//   });
 // });
+// // });
--- server.js
+++ server.js
@@ -55,8 +55,8 @@ function findMovie(search) {
 const confirmEntry = (array, input) => {
   for (let i = 0; i < 3; i++) {
     if (array[i].name.toLowerCase() === input.toLowerCase()) {
-      return array[0].name;
       // console.log(array[i].location.address1);
+      console.log(array[0].name);
     } else {
       console.log("Not found");
     }
@@ -119,97 +119,99 @@ app.get("/smart", (req, res) => {
 
 app.post("/smart", (req, res) => {
   yelpConfig.params.term = req.body.search; // Find restaurants
+  let foodResults = { class: "foodDisplay", value: "" };
+
   axios
     .get("https://api.yelp.com/v3/businesses/search", yelpConfig)
     .then(response => {
       let businessList = response.data.businesses;
-      confirmEntry(businessList, req.body.search);
-      res.send();
+      // console.log(businessList[i].location.address1);
+      foodResults.value = businessList[0].name;
     })
     .catch(error => {
       console.log("Error!");
     });
 
   // ----------------------------------------------------------------------------------------------------------
-  // const findMovie = search => {
-  //   // Find movies
-  //   imdb
-  //     .search(
-  //       {
-  //         title: `${req.body.search}`
-  //       },
-  //       {
-  //         apiKey: "b1b27127"
-  //       }
-  //     )
-  //     .then(result => {
-  //       let movieArray = result.results;
-  //       for (let searchResult of movieArray) {
-  //         if (
-  //           searchResult.title.toLowerCase() === req.body.search.toLowerCase()
-  //         ) {
-  //           res.send(searchResult.title + " " + searchResult.year);
-  //         }
-  //       }
-  //     })
-  //     .catch(console.log);
-  // };
+  let watchResult = { class: "watchDisplay", value: "" };
+  const findMovie = search => {
+    // Find movies
+    imdb
+      .search(
+        {
+          title: `${req.body.search}`
+        },
+        {
+          apiKey: "b1b27127"
+        }
+      )
+      .then(result => {
+        let movieArray = result.results;
+        for (let searchResult of movieArray) {
+          if (
+            searchResult.title.toLowerCase() === req.body.search.toLowerCase()
+          ) {
+            watchResult.value = searchResult.title + " " + searchResult.year;
+          }
+        }
+      })
+      .catch(console.log);
+  };
 
-  // findMovie(req.body.search);
+  findMovie(req.body.search);
 
   // // ---------------------------------------------------------------------------------------------------------------
-  // const paramsBooks = {
-  //   keywords: `${req.body.search}, Books`, // Search Ebay for Books
-  //   domainFilter: [{ name: "domainName", value: "Books" }]
-  // };
+  const paramsBooks = {
+    keywords: `${req.body.search}, Books`, // Search Ebay for Books
+    domainFilter: [{ name: "domainName", value: "Books" }]
+  };
+  let bookResults = { class: "bookDisplay", value: "" };
 
-  // ebay.xmlRequest(
-  //   {
-  //     serviceName: "Finding",
-  //     opType: "findItemsByKeywords",
-  //     appId: ebayID,
-  //     params: paramsBooks,
-  //     parser: ebay.parseResponseJson
-  //   },
-  //   // gets all the items together in a merged array
-  //   function itemsCallback(error, itemsResponse) {
-  //     if (error) throw error;
+  ebay.xmlRequest(
+    {
+      serviceName: "Finding",
+      opType: "findItemsByKeywords",
+      appId: ebayID,
+      params: paramsBooks,
+      parser: ebay.parseResponseJson
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw error;
 
-  //     let items = itemsResponse.searchResult.item;
+      let items = itemsResponse.searchResult.item;
+      bookResults.value = items[0].title;
 
-  //     console.log("Found", items.length, "items");
-
-  //     for (let i = 0; i < items.length; i++) {
-  //       res.send(items[i].primaryCategory);
-  //       // console.log(items[i].primaryCategory);
-  //     }
-  //   }
-  // );
+      // console.log(items[i].primaryCategory);
+    }
+  );
   // //---------------------------------------------------------------------------------------
+  const paramsProduct = {
+    keywords: `${req.body.search}`, // Search Ebay for products
+    domainFilter: [{ name: "domainName", value: "Books" }]
+  };
+  let merchResults = { class: "merchDisplay", value: "" };
 
-  // const paramsProduct = {
-  //   keywords: `${req.body.search}`, // Search Ebay for products
-  //   domainFilter: [{ name: "domainName", value: "Books" }]
-  // };
+  ebay.xmlRequest(
+    {
+      serviceName: "Finding",
+      opType: "findItemsByKeywords",
+      appId: ebayID,
+      params: paramsProduct,
+      parser: ebay.parseResponseJson
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw error;
 
-  // ebay.xmlRequest(
-  //   {
-  //     serviceName: "Finding",
-  //     opType: "findItemsByKeywords",
-  //     appId: ebayID,
-  //     params: paramsProduct,
-  //     parser: ebay.parseResponseJson
-  //   },
-  //   // gets all the items together in a merged array
-  //   function itemsCallback(error, itemsResponse) {
-  //     if (error) throw error;
-
-  //     let items = itemsResponse.searchResult.item;
-  //     console.log("Found", items.length, "items");
-  //     //   for (let i = 0; i < items.length; i++) {}
-  //     res.send({ item: items[0] });
-  //   }
-  // );
+      let items = itemsResponse.searchResult.item;
+      //   for (let i = 0; i < items.length; i++) {}
+      merchResults.value = items[0].title;
+    }
+  );
+  res.send(
+    testingvalue /*[merchResults, bookResults, foodResults, watchResult]*/
+  );
 });
 
 app.listen(PORT, () => {
