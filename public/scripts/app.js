71fbc9628392bee26c8fe3e2234ed75a80fc533c
camueljakcson <your@email.com>
2018-05-06 19:15:18 +0000

    links between pages added, login/register form, images resized and position

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,53 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
 
-  $('.toggleLogin').on('click', function() {
-    $('.loginForm').slideDown()
+$(() => {
+
+  $(".submitTodo").on("submit", function(event) {
+    const formData = $("form").serialize();
+    event.preventDefault();
+    $.get("/smart", function(response) {
+      $.ajax({
+        url: "/smart",
+        method: "POST",
+        data: formData,
+        success: function(result) {
+          if (result.keyword === "eat") {
+            $(".foodDisplay")
+              .append($(`<li>${result.keyword} ${result.value}</li>`))
+              .hide()
+              .fadeIn(800, function() {});
+          } else if (result.keyword === "watch") {
+            $(".watchDisplay")
+              .append($(`<li>${result.keyword} ${result.value}</li>`))
+              .hide()
+              .fadeIn(800, function() {});
+          } else if (result.keyword === "buy") {
+            $(".merchDisplay")
+              .append($(`<li>${result.keyword} ${result.value}</li>`))
+              .hide()
+              .fadeIn(800, function() {});
+          } else if (result.keyword === "read") {
+            $(".bookDisplay")
+              .append($(`<li>${result.keyword} ${result.value}</li>`))
+              .hide()
+              .fadeIn(800, function() {});
+          }
+        },
+        error: function(error) {
+          console.log("ajax call fail");
+        }
+      });
+    });
+  }); //api verification
+
+    $(".toggleLogin").click(function() {
+    $(".loginForm").slideDown();
   });
 
-});
+  $('#watchID').on('hover', '.hoverNotDisplay', function() {
+    $('.hoverNotDisplay').toggleClass('hoverDisplay');
+}); // hover over images
 
+
+
+
+}); //doc ready
